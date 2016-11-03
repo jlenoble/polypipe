51cbf61ff8e85d6591a831035e3b77394691ccfe
Jason Lenoble <jason.lenoble@gmail.com>
2016-11-03 12:57:26 +0100

    Factored out most of polypipe.test.js logic

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,9 @@
 import gulp from 'gulp';
+import {noop} from 'gulp-util';
+import babel from 'gulp-babel';
+import rename from 'gulp-rename';
 import {Transform} from 'stream';
+import PolyPipe from '../src/polypipe';
 
 const DestroyableTransform = gulp.src('*.notfound').constructor;
 
@@ -7,3 +11,94 @@ export function isStream(stream) {
   return stream instanceof DestroyableTransform ||
     stream instanceof Transform;
 };
+
+export const testGlobs = [
+  'gulpfile.babel.js',
+  'test/**/*.js',
+  [
+    'gulpfile.babel.js',
+    'test/**/*.js',
+    'gulp/*.js',
+    '!gulp/globs.js'
+  ]
+];
+
+function refStream1(glb) {
+  const [fn, ...args] = this.values;
+  return gulp.src(glb).pipe(fn(...args));
+}
+
+function refStream2(glb) {
+  var stream2 = gulp.src(glb);
+  this.values.forEach(values => {
+    if (!Array.isArray(values)) {
+      values = [values];
+    }
+    const [fn, ...args] = values;
+    stream2 = stream2.pipe(fn(...args));
+  });
+  return stream2;
+}
+
+function instantiate1() {
+  return new PolyPipe(...this.values);
+}
+
+function instantiate2() {
+  return new PolyPipe(this.values);
+}
+
+export const argsAsPlugins = [
+  {
+    description: 'noop',
+    values: [noop]
+  },
+  {
+    description: 'babel',
+    values: [babel]
+  },
+  {
+    description: `rename, {suffix: '-renamed'}`,
+    values: [rename, {suffix: '-renamed'}]
+  }
+];
+
+argsAsPlugins.forEach(args => {
+  Object.assign(args, {
+    refStream: refStream1,
+    instantiate: instantiate1
+  });
+});
+
+export const argsAsArrayOfPlugins =   [
+  {
+    description: '[noop]',
+    values: [noop],
+  },
+  {
+    description: '[[noop]]',
+    values: [[noop]]
+  },
+  {
+    description: '[babel]',
+    values: [babel]
+  },
+  {
+    description: `[[rename, {suffix: '-renamed'}]]`,
+    values: [[rename, {suffix: '-renamed'}]]
+  },
+  {
+    description: `[noop, [rename, {suffix: '-renamed'}], [babel]]`,
+    values: [noop, [rename, {suffix: '-renamed'}], [babel]]
+  }
+];
+
+argsAsArrayOfPlugins.forEach(args => {
+  Object.assign(args, {
+    refStream: refStream2,
+    instantiate: instantiate2
+  });
+});
+
+export const allArgs = [argsAsPlugins, argsAsArrayOfPlugins].reduce(
+  (array, next) => array.concat(next));
--- test/polypipe.test.js
+++ test/polypipe.test.js
@@ -1,127 +1,27 @@
 import gulp from 'gulp';
-import {noop} from 'gulp-util';
-import babel from 'gulp-babel';
-import rename from 'gulp-rename';
 import {expect} from 'chai';
-import PolyPipe from '../src/polypipe';
-import {isStream} from './helpers';
+import {isStream, allArgs, testGlobs} from './helpers';
 import equalStreamContents from 'equal-stream-contents';
 
 describe('Testing PolyPipe', function() {
 
-  [
-    {
-      description: 'noop',
-      values: [noop]
-    },
-    {
-      description: 'babel',
-      values: [babel]
-    },
-    {
-      description: `rename, {suffix: '-renamed'}`,
-      values: [rename, {suffix: '-renamed'}]
-    }
-  ].forEach(initArgs => {
+  allArgs.forEach(args => {
 
-    describe(`Initializing PolyPipe with (${initArgs.description})`,
+    describe(`Initializing PolyPipe with (${args.description})`,
       function() {
 
-        const pipe = new PolyPipe(...initArgs.values);
-        var [fn, ...args] = initArgs.values;
-
-        ['gulpfile.babel.js', 'test/**/*.js', [
-          'gulpfile.babel.js',
-          'test/**/*.js',
-          'gulp/*.js',
-          '!gulp/globs.js'
-        ]].forEach(glb => {
+        const pipe = args.instantiate();
+        testGlobs.forEach(glb => {
 
           it(`Using with glob '${glb}' (method through)`, function() {
             var stream = pipe.through(gulp.src(glb));
             expect(isStream(stream)).to.be.true;
-
-            return equalStreamContents(stream,
-              gulp.src(glb).pipe(fn(...args)));
+            return equalStreamContents(stream, args.refStream(glb));
           });
 
           it(`Using with glob '${glb} (method plugin)'`, function() {
             var stream = gulp.src(glb).pipe(pipe.plugin());
-
-            return equalStreamContents(stream,
-              gulp.src(glb).pipe(fn(...args)));
-          });
-
-        });
-
-      });
-
-  });
-
-  [
-    {
-      description: '[noop]',
-      values: [noop]
-    },
-    {
-      description: '[[noop]]',
-      values: [[noop]]
-    },
-    {
-      description: '[babel]',
-      values: [babel]
-    },
-    {
-      description: `[[rename, {suffix: '-renamed'}]]`,
-      values: [[rename, {suffix: '-renamed'}]]
-    },
-    {
-      description: `[noop, [rename, {suffix: '-renamed'}], [babel]]`,
-      values: [noop, [rename, {suffix: '-renamed'}], [babel]]
-    }
-  ].forEach(initArgs => {
-
-    describe(`Initializing PolyPipe with (${initArgs.description})`,
-      function() {
-
-        const pipe = new PolyPipe(initArgs.values);
-
-        ['gulpfile.babel.js', 'test/**/*.js', [
-          'gulpfile.babel.js',
-          'test/**/*.js',
-          'gulp/*.js',
-          '!gulp/globs.js'
-        ]].forEach(glb => {
-
-          it(`Using with glob '${glb}' (method through)`, function() {
-            var stream = pipe.through(gulp.src(glb));
-            expect(isStream(stream)).to.be.true;
-
-            var stream2 = gulp.src(glb);
-            initArgs.values.forEach(values => {
-              if (!Array.isArray(values)) {
-                values = [values];
-              }
-              const [fn, ...args] = values;
-              stream2 = stream2.pipe(fn(...args));
-            });
-
-            return equalStreamContents(stream, stream2);
-          });
-
-          it(`Using with glob '${glb} (method plugin)'`, function() {
-            var stream = gulp.src(glb).pipe(pipe.plugin());
-
-            var stream2 = gulp.src(glb);
-            initArgs.values.forEach(values => {
-              if (!Array.isArray(values)) {
-                values = [values];
-              }
-              const [fn, ...args] = values;
-              stream2 = stream2.pipe(fn(...args));
-            });
-
-            return equalStreamContents(stream, stream2);
+            return equalStreamContents(stream, args.refStream(glb));
           });
 
         });
