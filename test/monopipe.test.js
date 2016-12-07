4f8e51f9adb0dfeea1522523a5cec7ea98ac8fb6
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-07 12:01:54 +0100

    Tested class MonoPipe

---
+++ test/monopipe.test.js
@@ -0,0 +1,36 @@
+import gulp from 'gulp';
+import {expect} from 'chai';
+import {isStream, testGlobs} from './common-helpers';
+import {argsAsPlugins} from './monopipe-helpers';
+import equalStreamContents from 'equal-stream-contents';
+
+describe('Testing class MonoPipe', function() {
+
+  argsAsPlugins.forEach(args => {
+
+    describe(`Initializing MonoPipe with (${args.description})`,
+      function() {
+
+        this.timeout(4000);
+
+        const pipe = args.instantiate();
+        testGlobs.forEach(glb => {
+
+          it(`Using with glob '${glb}' (method through)`, function() {
+            var stream = pipe.through(gulp.src(glb));
+            expect(isStream(stream)).to.be.true;
+            return equalStreamContents(stream, args.refStream(glb));
+          });
+
+          it(`Using with glob '${glb} (method plugin)'`, function() {
+            var stream = gulp.src(glb).pipe(pipe.plugin());
+            return equalStreamContents(stream, args.refStream(glb));
+          });
+
+        });
+
+      });
+
+  });
+
+});
