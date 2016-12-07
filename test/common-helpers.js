52e73ca43391b139ff267429c4ff73a79e0d4854
Jason Lenoble <jason.lenoble@gmail.com>
2016-12-07 12:00:42 +0100

    Extracted common helpers from helpers

---
+++ test/common-helpers.js
@@ -0,0 +1,20 @@
+import gulp from 'gulp';
+import {Transform} from 'stream';
+
+const DestroyableTransform = gulp.src('*.notfound').constructor;
+
+export function isStream(stream) {
+  return stream instanceof DestroyableTransform ||
+    stream instanceof Transform;
+};
+
+export const testGlobs = [
+  'gulpfile.babel.js',
+  'test/**/*.js',
+  [
+    'gulpfile.babel.js',
+    'test/**/*.js',
+    'gulp/*.js',
+    '!gulp/globs.js'
+  ]
+];
