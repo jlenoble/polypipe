6809616488adbf73cc6a6c0712f0c8bfe59d04d8
Jason Lenoble <jason.lenoble@gmail.com>
2017-08-17 21:48:59 +0200

    Made second hint optional

--- src/polypipe.js
+++ src/polypipe.js
@@ -3,39 +3,39 @@ import {PolytonFactory} from 'polyton';
 import combine from 'stream-combiner';
 
 const PolyPipe = PolytonFactory( // eslint-disable-line new-cap
-MonoPipe, ['object', 'literal'], undefined, {
-  preprocess: args => {
-    let array = [];
-    args.forEach(arg => {
-      const [arg0] = arg;
-      if (arg0.initArgs) {
-        array = array.concat(arg0.initArgs);
-      } else {
-        array.push(arg);
-      }
-    });
-    return array;
-  },
-  extend: {
-    plugin () {
-      return combine(this.map(pipe => pipe.plugin()));
-    },
-
-    through (stream) {
-      return [stream, ...this.elements].reduce((stream, pipe) => {
-        return stream.pipe(pipe.plugin());
+  MonoPipe, ['object', {type: 'literal', optional: true}], undefined, {
+    preprocess: args => {
+      let array = [];
+      args.forEach(arg => {
+        const [arg0] = arg;
+        if (arg0.initArgs) {
+          array = array.concat(arg0.initArgs);
+        } else {
+          array.push(arg);
+        }
       });
+      return array;
     },
+    extend: {
+      plugin () {
+        return combine(this.map(pipe => pipe.plugin()));
+      },
 
-    pipe (...args) {
-      return this.concat(...args);
-    },
+      through (stream) {
+        return [stream, ...this.elements].reduce((stream, pipe) => {
+          return stream.pipe(pipe.plugin());
+        });
+      },
 
-    prepipe (...args) {
-      return (new PolyPipe(...args)).concat(this);
+      pipe (...args) {
+        return this.concat(...args);
+      },
+
+      prepipe (...args) {
+        return (new PolyPipe(...args)).concat(this);
+      },
     },
-  },
-});
+  });
 
 export default PolyPipe;
 export {MonoPipe};
